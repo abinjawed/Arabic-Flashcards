b7530359c640dc43faff7f3a836a3ed43e867b54
Awad Bin-Jawed <awadbinjawed@gmail.com>
2020-01-04 22:05:37 -0700

    Create ShapesActivity.kt

---
+++ Android App SDK/4.0/ShapesActivity.kt
@@ -0,0 +1,12 @@
+package emquart.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShapesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shapes)
+    }
+}
