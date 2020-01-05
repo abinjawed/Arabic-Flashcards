f5db3b13fe0839edc162a6c0e7ff71a3fd68a11d
Awad Bin-Jawed <awadbinjawed@gmail.com>
2020-01-04 22:05:08 -0700

    Create PeopleActivity.kt

---
+++ Android App SDK/4.0/PeopleActivity.kt
@@ -0,0 +1,12 @@
+package emquart.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PeopleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_people)
+    }
+}
