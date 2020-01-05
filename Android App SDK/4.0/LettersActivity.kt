e4e79d05e8c678350330cdb9a8551c8ab36bb6f0
Awad Bin-Jawed <awadbinjawed@gmail.com>
2020-01-04 22:03:40 -0700

    Create LettersActivity.kt

---
+++ Android App SDK/4.0/LettersActivity.kt
@@ -0,0 +1,12 @@
+package emquart.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LettersActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_letters)
+    }
+}
