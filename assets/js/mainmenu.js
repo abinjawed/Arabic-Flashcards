c7dfa405e6461febc8507156760f99ce035ea0af
Awad Bin-Jawed <awadbinjawed@gmail.com>
2020-06-06 09:45:47 -0600

    Add files via upload

---
+++ assets/js/mainmenu.js
@@ -0,0 +1,9 @@
+
+//the onClick listener to display coordinates in the console
+function getCoords(pointobject){
+var x = pointobject.clientX;
+var y = pointobject.clientY;
+
+console.log("x: " + x + " y: " + y);
+
+}
